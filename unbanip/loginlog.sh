c463590cc36a642a0d82b4be37f900614e0edab5
A. D. R. Lakshani <22644135+srkrose@users.noreply.github.com>
2024-06-17 15:20:54 +0530

    Update loginlog.sh

--- unbanip/loginlog.sh
+++ unbanip/loginlog.sh
@@ -6,7 +6,7 @@ ip=$1
 type=$2
 
 function log_data() {
-	cat /usr/local/cpanel/logs/login_log | grep "$ip" | grep "FAILED LOGIN" | awk '{print $1,$2,$5,$6,$8,$9}' | sed 's/[][]//g;s/"//' | awk '{printf "%-19s %-17s %-19s %-13s %-22s %-50s\n","DATE: "$1,"TIME: "$2,"LOGIN: "$3,"TYPE: "$6,"IP: "$4,"USER: "$5}' | sort | uniq -c >>$temp/$type-unban_$time.txt
+	cat /usr/local/cpanel/logs/login_log | grep "$ip" | grep "FAILED LOGIN" | awk '{printf "%-19s %-17s %-19s %-13s %-22s %-50s\n","DATE: "$1,"TIME: "$2,"LOGIN: "$5,"TYPE: "$9,"IP: "$6,"USER: "$8}' | sed 's/[][]//g;s/"//g' | uniq -c >>$temp/$type-unban_$time.txt
 }
 
 function filter_log() {
