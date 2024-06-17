5f8dfaeea28889a49f6fc5aa3d939ff25b2d332c
A. D. R. Lakshani <22644135+srkrose@users.noreply.github.com>
2024-06-17 15:14:19 +0530

    Update cphulk.sh

--- unbanip/cphulk.sh
+++ unbanip/cphulk.sh
@@ -3,47 +3,72 @@
 source /home/sample/scripts/dataset.sh
 
 ip=$1
+type=$2
 
 function ch_reason() {
     blacklist=$(whmapi1 read_cphulk_records list_name='black' | grep "$ip")
 
-    history=$(whmapi1 get_cphulk_excessive_brutes | grep -B 3 -A 3 "$ip" | awk '!seen[$0]++')
-
     if [[ ! -z "$blacklist" ]]; then
-        echo "Reason:"
+        category="blacklist"
 
         reason=$(egrep "$ip" $svrlogs/cphulk/iplist/* | cut -d' ' -f2-)
 
-        echo "$reason"
-
-        read -p "Remove from cPHulk Blacklist (y/n)? " answer
-
-        if [[ $answer == "y" || $answer == "Y" ]]; then
-            sh $scripts/unbanip/chunblock.sh $ip
-
-        fi
-
-    elif [[ -z "$blacklist" && ! -z "$history" ]]; then
-        echo "Reason:"
-
-        whois=$(echo "$history" | awk '{if($1=="country_code:") print $2}')
-        note=$(echo "$history" | awk '{if($1=="notes:") print}' | awk '{$1=""; print}' | sed 's/^[[:space:]]*//')
-
-        echo "$ip ($whois): $note"
-
-        type="cphulkd"
-
-        sh $scripts/unbanip/cphulkdlog.sh $ip $type
-
-        read -p "Flush from cPHulk History (y/n)? " answer
-
-        if [[ $answer == "y" || $answer == "Y" ]]; then
-            sh $scripts/unbanip/chunblock.sh $ip
-
-        fi
+        ch_unblock
 
     else
-        echo "cPHulk No records found"
+        cpbrute=$(whmapi1 get_cphulk_brutes | grep "$ip")
+        exbrute=$(whmapi1 get_cphulk_excessive_brutes | grep "$ip")
+        flogin=$(whmapi1 get_cphulk_failed_logins | grep "$ip")
+        ubrute=$(whmapi1 get_cphulk_user_brutes | grep "$ip")
+
+        category="history"
+
+        if [[ ! -z "$cpbrute" ]]; then
+            reason=$(whmapi1 get_cphulk_brutes | awk '/^data:/ {p=1; next} /^metadata:/ {p=0} p {print}' | awk '/^ *- *$/ {if (record != "") {print record; record=""}} /^[^-]/ {sub(/^ +/, "", $0); if (record == "") {record = $0} else {record = record " " $0}} END {if (record != "") {print record}}' | { read first_line; echo "$first_line"; grep "$ip"; })
+
+            ch_unblock
+
+        elif [[ ! -z "$exbrute" ]]; then
+            reason=$(whmapi1 get_cphulk_excessive_brutes | awk '/^data:/ {p=1; next} /^metadata:/ {p=0} p {print}' | awk '/^ *- *$/ {if (record != "") {print record; record=""}} /^[^-]/ {sub(/^ +/, "", $0); if (record == "") {record = $0} else {record = record " " $0}} END {if (record != "") {print record}}' | { read first_line; echo "$first_line"; grep "$ip"; })
+
+            ch_unblock
+
+        elif [[ ! -z "$flogin" ]]; then
+            reason=$(whmapi1 get_cphulk_failed_logins | awk '/^data:/ {p=1; next} /^metadata:/ {p=0} p {print}' | awk '/^ *- *$/ {if (record != "") {print record; record=""}} /^[^-]/ {sub(/^ +/, "", $0); if (record == "") {record = $0} else {record = record " " $0}} END {if (record != "") {print record}}' | { read first_line; echo "$first_line"; grep "$ip"; })
+
+            ch_unblock
+
+        elif [[ ! -z "$ubrute" ]]; then
+            reason=$(whmapi1 get_cphulk_user_brutes | awk '/^data:/ {p=1; next} /^metadata:/ {p=0} p {print}' | awk '/^ *- *$/ {if (record != "") {print record; record=""}} /^[^-]/ {sub(/^ +/, "", $0); if (record == "") {record = $0} else {record = record " " $0}} END {if (record != "") {print record}}' | { read first_line; echo "$first_line"; grep "$ip"; })
+
+            ch_unblock
+
+        else
+            echo "cPHulk No records found"
+        fi
+    fi
+}
+
+function ch_unblock() {
+    if [[ "$type" == "cphulk" ]]; then
+        echo "Blocked by cPHulk:"
+        
+    fi
+
+    echo "Reason:"
+
+    echo "$reason"
+
+    if [[ "$category" == "blacklist" ]]; then
+        read -p "Remove from cPHulk Blacklist (y/n)? " answer
+    else
+        echo "$reason" >>$svrlogs/unbanip/cphulk/$category-unban_$time.txt
+
+        read -p "Flush from cPHulk History (y/n)? " answer
+    fi
+
+    if [[ $answer == "y" || $answer == "Y" ]]; then
+        sh $scripts/unbanip/chunblock.sh $ip $category
     fi
 }
 
